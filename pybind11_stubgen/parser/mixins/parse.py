c3a6ef67ec23d5a39e8cdc73b2c3cd8c1794d6f2
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-10-29 11:49:24 +0000

    Save as __init__.pyi if __path__ exists (#2)
    * Updated gitignore
    
    * Save stub file to __init__.pyi if __path__ exists
    
    In python the existence of __path__ indicates that the module is a package.
    If this variable is present the module should be saved as __init__.py even if it has no submodules.

--- pybind11_stubgen/parser/mixins/parse.py
+++ pybind11_stubgen/parser/mixins/parse.py
@@ -107,6 +107,8 @@ class ParserDispatchMixin(IParser):
             elif isinstance(obj, TypeVar_):
                 result.type_vars.append(obj)
             elif obj is None:
+                if name == "__path__":
+                    result.is_package = True
                 pass
             else:
                 raise AssertionError()
